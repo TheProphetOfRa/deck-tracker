14347899502de292f918949f060e12dc986484e3
David Hodgkinson <lelunedor@gmail.com>
2020-07-10 18:59:32 +0100

    Initial Commit

---
+++ Shared/DeckTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeckTrackerApp.swift
+//  Shared
+//
+//  Created by David Hodgkinson on 10/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct DeckTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
